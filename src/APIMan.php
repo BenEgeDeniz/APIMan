3dea1ddaa57f63ce5899f96c0511520c29284d68
Ege Deniz <ege@benegedeniz.com>
2020-12-18 02:37:29 +0300

    Update APIMan.php

--- src/APIMan.php
+++ src/APIMan.php
@@ -5,105 +5,104 @@
  * Developer: TimberLock
  * Developer Website: benegedeniz.com
  */
-namespace BenEgeDeniz
+class APIMan
 {
-	class APIMan
+	private $endpoint;
+	private $headers;
+	private $data;
+	private $request;
+	private $sslConfig;
+	private $ch;
+	private $return;
+
+	function __construct(string $endpoint)
 	{
-		private $endpoint;
-		private $headers;
-		private $data;
-		private $request;
-		private $sslConfig;
-		private $ch;
-		private $return;
+		$this->endpoint = $endpoint;
+	}
 
-		function __construct(string $endpoint)
+	public function setSSLConfig(array $config)
+	{
+		$this->sslConfig = $config;
+	}
+
+	public function setHeaders(array $headers)
+	{
+		$this->headers = $headers;
+	}
+
+	public function setRequestType(string $type)
+	{
+		$supported = ["post", "get", "put", "delete"];
+
+		if (!in_array(strtolower($type), $supported))
+			throw new \Exception("Request type isn't supported (yet). Supported types: post, get, put, delete", 1);
+
+		$this->request = $type;
+	}
+
+	public function setData($data)
+	{
+		$this->data = $data;
+	}
+
+	public function executeRequest()
+	{
+		if (gettype($this->sslConfig['SSL_VERIFYPEER']) != "boolean" || gettype($this->sslConfig['SSL_VERIFYPEER']) != "boolean")
+			throw new \Exception("SSL configuration must be set correctly. Use setSSLConfig method to do that.", 1);
+
+		$this->ch = curl_init();
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+
+		if (!empty($this->headers))
+			curl_setopt($this->ch, CURLOPT_HTTPHEADER, $this->headers);
+
+		curl_setopt($this->ch, CURLOPT_HEADER, false);
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, $this->sslConfig['SSL_VERIFYPEER']);
+
+		if ($this->sslConfig['SSL_VERIFYHOST'] === true)
+			curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, 2);
+
+		if (strtolower($this->request) == "post")
 		{
-			$this->endpoint = $endpoint;
+			curl_setopt($this->ch, CURLOPT_URL, $this->endpoint);
+			curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "POST");
+
+			if (!empty($this->data))
+				curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->data);
 		}
 
-		public function setSSLConfig(array $config)
+		if (strtolower($this->request) == "get")
 		{
-			$this->sslConfig = $config;
+			curl_setopt($this->ch, CURLOPT_URL, $this->endpoint . "?" . $this->data);
+			curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "GET");
 		}
 
-		public function setHeaders(array $headers)
+		if (strtolower($this->request) == "put")
 		{
-			$this->headers = $headers;
+			curl_setopt($this->ch, CURLOPT_URL, $this->endpoint);
+			curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "PUT");
+
+			if (!empty($this->data))
+				curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->data);
 		}
 
-		public function setRequestType(string $type)
+		if (strtolower($this->request) == "delete")
 		{
-			$supported = ["post", "get", "put", "delete"];
+			curl_setopt($this->ch, CURLOPT_URL, $this->endpoint);
+			curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "DELETE");
 
-			if (!in_array(strtolower($type), $supported))
-				throw new \Exception("Request type isn't supported (yet). Supported types: post, get, put, delete", 1);
-
-			$this->request = $type;
+			if (!empty($this->data))
+				curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->data);
 		}
 
-		public function setData($data)
-		{
-			$this->data = $data;
-		}
+		$this->return = curl_exec($this->ch);
+		curl_close($this->ch);
+	}
 
-		public function executeRequest()
-		{
-			if (gettype($this->sslConfig['SSL_VERIFYPEER']) != "boolean" || gettype($this->sslConfig['SSL_VERIFYPEER']) != "boolean")
-				throw new \Exception("SSL configuration must be set correctly. Use setSSLConfig method to do that.", 1);
-
-			$this->ch = curl_init();
-			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
-
-			if (!empty($this->headers))
-				curl_setopt($this->ch, CURLOPT_HTTPHEADER, $this->headers);
-
-			curl_setopt($this->ch, CURLOPT_HEADER, false);
-			curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, $this->sslConfig['SSL_VERIFYPEER']);
-
-			if ($this->sslConfig['SSL_VERIFYHOST'] === true)
-				curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, 2);
-
-			if (strtolower($this->request) == "post")
-			{
-				curl_setopt($this->ch, CURLOPT_URL, $this->endpoint);
-				curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "POST");
-
-				if (!empty($this->data))
-					curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->data);
-			}
-
-			if (strtolower($this->request) == "get")
-			{
-				curl_setopt($this->ch, CURLOPT_URL, $this->endpoint . "?" . $this->data);
-				curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "GET");
-			}
-
-			if (strtolower($this->request) == "put")
-			{
-				curl_setopt($this->ch, CURLOPT_URL, $this->endpoint);
-				curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "PUT");
-
-				if (!empty($this->data))
-					curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->data);
-			}
-
-			if (strtolower($this->request) == "delete")
-			{
-				curl_setopt($this->ch, CURLOPT_URL, $this->endpoint);
-				curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-
-				if (!empty($this->data))
-					curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->data);
-			}
-
-			$this->return = curl_exec($this->ch);
-			curl_close($this->ch);
-		}
-
-		public function getRawResponse()
-		{
-			return $this->return;
-		}
+	public function getRawResponse()
+	{
+		return $this->return;
 	}
 }
+
+?>
