9a72457f2a4b1c05ae46d6e12873212539a4e02d
kacardenizge <59136798+kacardenizge@users.noreply.github.com>
2020-12-18 14:56:04 +0300

    Added phpDoc and optimized
    Added phpDoc.
    Optimized code.

--- src/apiman.php
+++ src/apiman.php
@@ -21,15 +21,67 @@ class APIMan
 		$this->endpoint = $endpoint;
 	}
 
+	/**
+	 *
+	 * setProxy
+	 *
+	 * This method will set proxy tunnel.
+	 * 
+	 * @param array $proxy This will be your proxy config.
+	 *
+	 * Example:
+	 * Array
+	 * (
+     *		[proxy] => proxyip:port
+     *		[proxyType] => {http, https, socks4, socks5}
+     *		[auth] => username:password (optional)
+	 *	)
+	 *
+	 * @return void
+	 *
+	 */
+
 	public function setProxy(array $proxy)
 	{
 		$this->proxyConfig = $proxy;
 	}
 
+	/**
+	 *
+	 * setHeaders
+	 *
+	 * This method will set http headers that you want to send.
+	 * 
+	 * @param array $headers Your header array.
+	 *
+	 * Example:
+	 * Array
+	 * (
+     *		[0] => Content-Type: application/json
+     *		[1] => User-Agent: APIMan
+	 *	)
+	 *
+	 * @return void
+	 *
+	 */
+
 	public function setHeaders(array $headers)
 	{
 		$this->headers = $headers;
 	}
+
+	/**
+	 *
+	 * setRequestType
+	 *
+	 * This method will set your request type.
+	 * 
+	 * @param string $type Your request type. Can be: get, post, put and delete.
+	 *
+	 * @return void (If satisfied.)
+	 * @return exception (If request type is invalid.)
+	 *
+	 */
 
 	public function setRequestType(string $type)
 	{
@@ -38,18 +90,61 @@ class APIMan
 		if (!in_array(strtolower($type), $supported))
 			throw new \Exception("Request type isn't supported (yet). Supported types: post, get, put, delete", 1);
 
+		unset($supported);
+
 		$this->request = $type;
 	}
+
+	/**
+	 *
+	 * setData
+	 *
+	 * This method will set your request type.
+	 * 
+	 * @param mixed $data Your data to send. This can be a query or raw body. You can not use raw body with get!
+	 *
+	 * @return void
+	 *
+	 */
 
 	public function setData($data)
 	{
 		$this->data = $data;
 	}
 
+	/**
+	 *
+	 * setSSLConfig
+	 *
+	 * This method will set SSL configuration.
+	 * 
+	 * @param array $config Your SSL configuration array. This is required.
+	 *
+	 * Example:
+	 * Array
+	 * (
+     *		[SSL_VERIFYPEER] => (bool)
+     *		[SSL_VERIFYHOST] => (bool)
+	 *	)
+	 *
+	 * @return void
+	 *
+	 */
+
 	public function setSSLConfig(array $config)
 	{
 		$this->sslConfig = $config;
 	}
+
+	/**
+	 *
+	 * executeRequest
+	 *
+	 * This method will execute your request with your set parameters.
+	 *
+	 * @return void
+	 *
+	 */
 
 	public function executeRequest()
 	{
@@ -58,6 +153,14 @@ class APIMan
 
 		$this->ch = curl_init();
 		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->ch, CURLOPT_HEADER, false);
+
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, $this->sslConfig['SSL_VERIFYPEER']);
+
+		if ($this->sslConfig['SSL_VERIFYHOST'] === true)
+			curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, 2);
+
+		unset($this->sslConfig);
 
 		if (isset($this->proxyConfig['proxyType']) || isset($this->proxyConfig['proxy']))
 		{
@@ -80,16 +183,14 @@ class APIMan
 
 			if (isset($this->proxyConfig['auth']))
 				curl_setopt($this->ch, CURLOPT_PROXYUSERPWD, $this->proxyConfig['auth']);
+
+			unset($this->proxyConfig);
 		}
 
 		if (!empty($this->headers))
 			curl_setopt($this->ch, CURLOPT_HTTPHEADER, $this->headers);
 
-		curl_setopt($this->ch, CURLOPT_HEADER, false);
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, $this->sslConfig['SSL_VERIFYPEER']);
-
-		if ($this->sslConfig['SSL_VERIFYHOST'] === true)
-			curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, 2);
+		unset($this->headers);
 
 		if (strtolower($this->request) == "post")
 		{
@@ -124,13 +225,29 @@ class APIMan
 				curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->data);
 		}
 
+		unset($this->data, $this->request, $this->endpoint);
+
 		$this->return = curl_exec($this->ch);
 		curl_close($this->ch);
+
+		unset($this->ch);
 	}
+
+	/**
+	 *
+	 * getRawResponse
+	 *
+	 * This method will return the response from your API endpoint.
+	 *
+	 * @return Response from your API endpoint.
+	 *
+	 */
 
 	public function getRawResponse()
 	{
 		return $this->return;
+
+		unset($this->return);
 	}
 }
 
